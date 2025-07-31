5b53e15fd8c68d4fd0041d166f1c2d638279d52e
Sudotec <henriquepedro.piola@gmail.com>
2025-07-30 22:11:23 -0300

    Git Comitado

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/usuarios", (req, res) => {
+  res.status(200).json({menssagem: 'Rota de Get - usuarios'});
+});
+
+app.get('/usuarios/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({menssagem: `Rota de Get - usuarios - ${id}`});
+});
+
+app.post('/usuarios/criar', (req, res) => {
+  const { nome, email } = req.body;
+  res.status(201).json({menssagem: `Rota de Post - usuarios - ${nome} - ${email}`});
+});
+
